ec916fe211ca3609ba8fcfed77dcedffcea8032c
FlorianEBI <florian.ebi@gmail.com>
2014-08-19 11:06:44 +0000

    swagger documentation update

--- src/main/java/uk/ac/ebi/pride/archive/web/service/controller/WelcomeController.java
+++ src/main/java/uk/ac/ebi/pride/archive/web/service/controller/WelcomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @since $version
  */
 @Controller
+@ApiIgnore
 @RequestMapping(value = {"", "/"})
 public class WelcomeController {
 
